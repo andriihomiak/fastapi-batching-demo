c641c3aa2abe0561e33b5486bcacab3698d712cf
Andrii Khomiak <homiakandrii@gmail.com>
2022-07-29 19:16:30 +0300

    feat: Cleanup

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import asyncio
 import logging
 import random
-from asyncio.log import logger
 from datetime import datetime
 from typing import List, Tuple
-from uuid import uuid4
 
-from fastapi import FastAPI, Request, status
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 import dispatcher
@@ -18,46 +14,28 @@ class Response(BaseModel):
     start: datetime
     value: float
     data: str
-    
-
 
 
 logger = logging.getLogger(__name__)
-
-shared_semaphore = dispatcher.NonBlockSema(1)
-
-@dispatcher.CorkDispatcher(max_latency_in_ms=5000 * 1000, max_batch_size=50, shared_sema=shared_semaphore)
-async def batch_me(data: Tuple[str]) -> datetime:
-    data = list(data)
-    # logger.info(f"Got inference request: {data}")
-    start = datetime.now()
-    delay = random.random()
-    await asyncio.sleep(0)
-    # logger.info(f"start: {start}, value: {delay}, data: {data}")
-    responses = [Response(start=start, value=delay, data=d) for d in data]
-    # logger.info(f"Got result: {responses}")
-    return responses
-
 logging.basicConfig(level=logging.DEBUG)
 
+
+@dispatcher.CorkDispatcher(max_latency_in_ms=5000 * 1000, max_batch_size=50)
+async def batch_computation(data: Tuple[str]) -> datetime:
+    # simulate log running batch computation (e.g. ML model inference)
+    data = list(data)
+    start = datetime.now()
+    delay = random.random()
+    await asyncio.sleep(0.5)
+    responses = [Response(start=start, value=delay, data=d) for d in data]
+    return responses
+
 app = FastAPI()
-
-
-
-app = FastAPI()
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
-    logging.error(f"{exc_str}")
-    content = {'status_code': 10422, 'message': exc_str, 'data': None}
-    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @app.post("/", response_model=List[Response])
-async def enpoint(data: List) -> List[Response]:
-    tasks = [
-        batch_me(i)
-        for i in data
-    ]
+async def batchable_endpoint(data: List) -> List[Response]:
+    # need to pass single item at a time,
+    # batching is done by the dispatcher
+    tasks = [batch_computation(i) for i in data]
     return await asyncio.gather(*tasks)
